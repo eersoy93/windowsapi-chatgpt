352a8d7c5de693838fc50b2ce90916ecc406f676
Erdem Ersoy <erdemersoy256@gmail.com>
2023-02-13 08:48:34 +0300

    Create an empty window with an message box
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- WindowsAPIChatGPT/WindowsAPIChatGPT.c
+++ WindowsAPIChatGPT/WindowsAPIChatGPT.c
@@ -1,8 +1,56 @@
 #include <Windows.h>
 #include <stdlib.h>
 
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CLOSE:
+        PostQuitMessage(0);
+        break;
+    case WM_KEYDOWN:
+        if (wParam == VK_SPACE)
+        {
+            MessageBox(hWnd, L"Hello, World!", L"Hello, World!", MB_OK | MB_ICONEXCLAMATION);
+        }
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	MessageBox(NULL, L"Hello, World!", L"Hello, world!", MB_OK | MB_ICONEXCLAMATION);
-	return EXIT_SUCCESS;
+    WNDCLASSEX wc = { 0 };
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_3DFACE + 1);
+    wc.lpszClassName = L"WindowClass";
+
+    RegisterClassEx(&wc);
+
+    HWND hWnd = CreateWindowEx(0, L"WindowClass", L"Hello, World!", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL, NULL, hInstance, NULL);
+
+    if (!hWnd)
+        return 1;
+
+    ShowWindow(hWnd, nCmdShow);
+
+    MSG msg = { 0 };
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
 }
